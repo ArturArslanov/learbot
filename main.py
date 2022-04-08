7d0b0ec3e6a56b3d99727f5a48f6963c0ea232db
Artur <aat281205@gmail.com>
2022-04-08 21:33:20 +0300

    /timer /date /set_timer123

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import logging
+import random
+
 import my_config as config
-from telegram import Update, ForceReply
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+from telegram import Update, ForceReply, ReplyKeyboardRemove, ReplyKeyboardMarkup, \
+    InlineKeyboardMarkup, InlineKeyboardButton
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext, \
+    CallbackQueryHandler
 from datetime import datetime
 
 logging.basicConfig(
@@ -14,7 +18,7 @@ logger = logging.getLogger(__name__)
 def task(context):
     """Выводит сообщение"""
     job = context.job
-    context.bot.send_message(job.context, text='КУКУ!')
+    context.bot.send_message(job.context, text='время вышло')
 
 
 def remove_job_if_exists(name, context):
@@ -32,7 +36,7 @@ def unset(update, context):
     """Удаляет задачу, если пользователь передумал"""
     chat_id = update.message.chat_id
     job_removed = remove_job_if_exists(str(chat_id), context)
-    text = 'Таймер отменен!' if job_removed else 'У вас нет активных таймеров'
+    text = 'Таймер отменен!' if job_removed else 'таймер выключен'
     update.message.reply_text(text)
 
 
@@ -59,8 +63,8 @@ def set_timer(update, context):
 
 def start(update: Update, context: CallbackContext):
     user = update.effective_user
-    update.message.reply_text(f"""Здравствуйте {user.mention_markdown_v2()}!""",
-                              reply_markup=ForceReply(selective=True), )
+    update.message.reply_text(f"""Здравствуйте {user.mention_markdown_v2()}!
+    Я бот-помощник для настольных игр""", reply_markup=game_markup, )
 
 
 def help_command(update: Update, context: CallbackContext):
@@ -88,6 +92,48 @@ def today_date(update: Update, context: CallbackContext):
     update.message.reply_text(f"год - {year}, месяц - {month}, день - {day}")
 
 
+def dice(update: Update, context: CallbackContext):
+    keyboard = [
+        [InlineKeyboardButton('кинуть шестигранный кубик', callback_data='6'),
+         InlineKeyboardButton('кинуть 2 шестигранных кубика одновременно', callback_data='2x6')],
+        [InlineKeyboardButton('кинуть 20-гранный кубик', callback_data='20'),
+         InlineKeyboardButton('вернуться назад', callback_data='back')]
+    ]
+    markup = InlineKeyboardMarkup(keyboard)
+    update.message.reply_text('игра', reply_markup=markup)
+
+
+def timer(update: Update, context: CallbackContext):
+    keyboard = [
+        [InlineKeyboardButton('30 секунд', callback_data='/set 30'),
+         InlineKeyboardButton('1 минута', callback_data='/set 60')],
+        [InlineKeyboardButton('5 минут', callback_data='/set 300'),
+         InlineKeyboardButton('вернуться назад', callback_data='back')]
+    ]
+    markup = InlineKeyboardMarkup(keyboard)
+    update.message.reply_text('засечь', reply_markup=markup)
+
+
+def button(update: Update, context: CallbackContext) -> None:
+    query = update.callback_query
+    query.answer()
+    if query.data == 'back':
+        query.delete_message()
+    if query.data == 'close':
+        unset(query, context)
+        query.delete_message()
+    if query.data.startswith('/set'):
+        keyboard = [[InlineKeyboardButton('close', callback_data='close')]]
+        markup = InlineKeyboardMarkup(keyboard)
+        query.message.reply_text(f'скопируйте {query.data}', reply_markup=markup)
+    if query.data == '6':
+        query.message.reply_text(text=f"выпало {random.randint(0, 6)}")
+    if query.data == '2x6':
+        query.message.reply_text(text=f"выпало {random.randint(0, 6)} и {random.randint(0, 6)}")
+    if query.data == '20':
+        query.message.reply_text(text=f"выпало {random.randint(0, 20)}")
+
+
 def main():
     uper = Updater(config.TOKEN)
     dispatcher = uper.dispatcher
@@ -100,15 +146,20 @@ def main():
                                           pass_job_queue=True,
                                           pass_chat_data=True))
     dispatcher.add_handler(CommandHandler("unset", unset,
-                                          pass_chat_data=True)
-                           )
+                                          pass_chat_data=True))
+    dispatcher.add_handler(CommandHandler("dice", dice))
+    uper.dispatcher.add_handler(CallbackQueryHandler(button))
 
     uper.start_polling()
 
     uper.idle()
 
 
-commands = {'/start': start, '/help': help_command, '/time': time_now, '/date': today_date}
+commands = {'/start': start, '/help': help_command, '/time': time_now, '/date': today_date,
+            '/dice': dice, '/timer': timer}
 spec_commands = ['/set', '/unset']
+game_keyboard = [['/dice', '/timer']]
+game_markup = ReplyKeyboardMarkup(game_keyboard, one_time_keyboard=True)
+
 if __name__ == "__main__":
     main()
